ec6f6447169b9daec954af44ad778ea5c4e5127a
黄雨生 <huangyusheng@gmail.com>
2017-07-26 16:29:59 +0800

    增加Label扩展

--- Mood/LabelExtension.swift
+++ Mood/LabelExtension.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class LabelExtension: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.numberOfLines = 0
+        self.lineBreakMode = .byTruncatingTail
+        self.sizeToFit()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.numberOfLines = 0
+        self.lineBreakMode = .byTruncatingTail
+        self.sizeToFit()
+    }
 }
