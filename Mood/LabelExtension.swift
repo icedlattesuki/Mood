20af91f5aeb53ff6b083e3c1b0f873835abca4b4
黄雨生 <huangyusheng@gmail.com>
2017-07-26 16:29:23 +0800

    更新添加心情视图控制器

---
+++ Mood/LabelExtension.swift
@@ -0,0 +1,21 @@
+//
+//  LabelExtension.swift
+//  Mood
+//
+//  Created by Hys on 2017/7/26.
+//  Copyright © 2017年 Hys. All rights reserved.
+//
+
+import UIKit
+
+class LabelExtension: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
