9fd8bf1b6a1c9a649e160d01fea81aefef72715a
黄雨生 <huangyusheng@gmail.com>
2017-07-26 16:30:19 +0800

    更新TabBar控制器

--- Mood/MainTabBarController.swift
+++ Mood/MainTabBarController.swift
@@ -8,27 +8,12 @@
 
 import UIKit
 
-class MainTabBarController: UITabBarController {
-
+class MainTabBarController: UITabBarController{
+    
+    //MARK: Override functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
