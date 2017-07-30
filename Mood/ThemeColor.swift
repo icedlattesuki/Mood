b4e08d83b56acb1cd90dadacbae484c8e335d8d2
ShengAndXian <huangyusheng01@gmail.com>
2017-07-30 23:55:59 +0800

    增加ThemeColor

---
+++ Mood/ThemeColor.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeColor.swift
+//  Mood
+//
+//  Created by Hys on 2017/7/30.
+//  Copyright © 2017年 Hys. All rights reserved.
+//
+
+import UIKit
+
+struct ThemeColor {
+    static let red = CGFloat(26/255.0)
+    static let green = CGFloat(188/255.0)
+    static let blue = CGFloat(156/255.0)
+    static let alpha = CGFloat(0.1)
+}
