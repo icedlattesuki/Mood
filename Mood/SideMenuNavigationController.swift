7cda0fd13c6066f675a6e11e11d5b4247946703e
ShengAndXian <huangyusheng01@gmail.com>
2017-07-31 23:27:43 +0800

    增加SideMenuNavigationController

---
+++ Mood/SideMenuNavigationController.swift
@@ -0,0 +1,26 @@
+//
+//  SideMenuNavigationController.swift
+//  Mood
+//
+//  Created by Hys on 2017/7/30.
+//  Copyright © 2017年 Hys. All rights reserved.
+//
+
+import UIKit
+import Material
+import SideMenu
+
+class SideMenuNavigationController: UISideMenuNavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationBar.barTintColor = UIColor(red: ThemeColor.red, green: ThemeColor.green, blue: ThemeColor.blue, alpha: ThemeColor.alpha)
+        self.navigationBar.isTranslucent = true
+        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: Color.white]
+        
+        SideMenuManager.menuWidth = 350
+        SideMenuManager.menuPresentMode = .viewSlideInOut
+        SideMenuManager.menuFadeStatusBar = false
+    }
+}
